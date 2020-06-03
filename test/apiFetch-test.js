bcdacba1292f3bcdf3c17a4ae4a7c6e3cb75d294
emontealvo <edwin.montealvo@gmail.com>
2020-06-03 17:41:07 -0600

    Add test for initial structure

--- test/apiFetch-test.js
+++ test/apiFetch-test.js
@@ -1,5 +1,8 @@
 import chai from 'chai';
 const expect = chai.expect;
+const spies = require('chai-spies');
+chai.use(spies)
+
 import ApiFetch from '../src/main/apiFetch'
 
 describe ('Api fetch class', function() {
@@ -17,9 +20,24 @@ describe ('Api fetch class', function() {
   });
 
   it('should have our api\'s root url as default', function() {
-    let url = "https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/"
+    let url = "https://fe-apps.herokuapp.com/api/v1/travel-tracker/data"
     let apiFetch = new ApiFetch();
 
     expect(apiFetch.rootUrl).to.equal(url)
-  })
-})
+  });
+});
+
+describe.skip('Api fetch behvior', function() {
+
+  beforeEach(function() {
+    
+    global.window = {}; 
+    chai.spy.on(window, 'fetch', () => {});
+  });
+
+  it('should be able to fetch traveler Data', function() {
+
+    let apiFetch = new ApiFetch();
+    expect(apiFetch.getTravelerData()).to.call(window.fetch())
+  });
+});
