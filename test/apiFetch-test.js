af1a173e8e0cf8c12c4ccc2efba2c20afac14a66
emontealvo <edwin.montealvo@gmail.com>
2020-06-03 17:19:50 -0600

    Add initial api fetch class structure

--- test/apiFetch-test.js
+++ test/apiFetch-test.js
@@ -1,2 +1,25 @@
 import chai from 'chai';
 const expect = chai.expect;
+import ApiFetch from '../src/main/apiFetch'
+
+describe ('Api fetch class', function() {
+
+  it('should be a function', function() {
+    let apiFetch = new ApiFetch();
+
+    expect(ApiFetch).to.be.a('function');
+  });
+
+  it('should create an instance of an ApiClass', function() {
+    let apiFetch = new ApiFetch();
+
+    expect(apiFetch).to.be.an.instanceof(ApiFetch)
+  });
+
+  it('should have our api\'s root url as default', function() {
+    let url = "https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/"
+    let apiFetch = new ApiFetch();
+
+    expect(apiFetch.rootUrl).to.equal(url)
+  })
+})
