1c50c7363f572956465f67cca723997275f98524
emontealvo <edwin.montealvo@gmail.com>
2020-06-04 14:20:10 -0600

    Add more test to further user parent class functionality

--- test/user-test.js
+++ test/user-test.js
@@ -6,22 +6,35 @@ describe('User Class', () => {
 
   let user;
   beforeEach(() => {
-    user = new User()
-  })
+    user = new User();
+  });
 
   it('should be a function', () => {
-    expect(User).to.be.a('function')
+    expect(User).to.be.a('function');
   });
 
   it('should create an User instance', () => {
-    expect(user).to.be.an.instanceof(User)
+    expect(user).to.be.an.instanceof(User);
   });
 
   it('should have a type', () => {
-    expect('type').to.be.a.property.of(user)
+    expect('type').to.be.a.property.of(user);
   });
 
   it('should default to a guest user type', () => {
-    expect(user.type).to.equal('Guest')
-  })
-})
+    expect(user.type).to.equal('Guest');
+  });
+
+  it('should be able to be other types of user', () => {
+    user = new User('Traveler');
+    expect(user.type).to.equal('Traveler');
+  });
+
+  it('should hold onto trips', () => {
+    expect('trips').to.be.a.property.of(user);
+  });
+
+  it('should default to an empty array if no trips are given', () => {
+    expect(user.trips).to.deep.equal([]);
+  });
+});
